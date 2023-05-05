288f60076aa41537c8470581796e693e5dceef01
DeFreitasFlavio <flavio.defreitas@ynov.com>
2023-05-05 21:06:47 +0200

    add one Projets

--- src/components/Projets/Projets.jsx
+++ src/components/Projets/Projets.jsx
@@ -6,22 +6,31 @@ const Projets = () => {
     {
       id: "1",
       title: "ShopTaBoard",
-      details: "",
+      details:
+        "This is a skateboard E-commerce, developed in flutter with GetX and Dio libraries for the front-end, and for the back-end we used NestJs with PostgresSql.",
+      created: "Graig, Bastien, Flavio",
+      git: "https://github.com/Gra1gkolodziejczyk/flutter_ecommerce_getx/tree/feature/home_page",
     },
     {
       id: "2",
       title: "Twitter Clone",
       details: "",
+      created: "Graig, Flavio",
+      git: "",
     },
     {
       id: "3",
       title: "Top-Down Shooter",
       details: "",
+      created: "Flavio",
+      git: "",
     },
     {
       id: "4",
       title: "PortraiScopie",
       details: "",
+      created: "Graig, Flavio",
+      git: "",
     },
   ];
   return (
@@ -35,11 +44,13 @@ const Projets = () => {
             return (
               <div className="Projets">
                 <p className="MediumText">{post.title}</p>
-                <p>
-                  {post.details}
-                  Explication bref de ce que c'est et ce que fait le projet. en
-                  Anglais
-                </p>
+                <p>{post.details}</p>
+                <p>Created by {post.created}</p>
+                <div className="Button">
+                  <a href={post.git} className="ButtonProjets">
+                    Github
+                  </a>
+                </div>
               </div>
             );
           })}

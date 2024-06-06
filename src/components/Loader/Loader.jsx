ad817d1bc56983d0c0d8742796b12fe49576bbb7
Flavio De Freitas <flaviodefreitas@MacBook-Pro-de-Flavio.local>
2024-06-06 08:48:01 +0200

    Fix loader and UX

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,7 @@
 import { React, useState, useEffect } from "react";
 import "./Loader.css";
-
-import { Player } from "@lottiefiles/react-lottie-player";
+import loaders from "../../assets/Loader.json";
+import Lottie from "lottie-react";
 
 import Homepage from "../Homepage/Homepage.jsx";
 
@@ -18,20 +18,7 @@ const Loader = () => {
     <>
       {loading ? (
         <div className="Loading">
-          <Player
-            autoplay
-            loop
-            src="https://lottie.host/a07a2d86-686c-4ab5-9c27-4ee52e860989/wBZeqo9vFH.json"
-            style={{
-              height: "480px",
-              width: "480px",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            speed={2}
-          ></Player>
+          <Lottie animationData={loaders} loop={true} />
         </div>
       ) : (
         <Homepage />
